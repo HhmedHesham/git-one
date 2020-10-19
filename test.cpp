4644b66d7a4c52916389ae6d332bd053d34f163d
iamahmedh <heshama793@gmail.com>
2020-10-19 23:29:33 +0200

    3 adding gender condition

--- test.cpp
+++ test.cpp
@@ -3,15 +3,24 @@
 using namespace std;
 
 int main() {
+
 	int result{ 0 };
-	
 	cout << "Enter your result to know your Grade\n";
 	cin >> result;
-	
+
+	char gender{};
+	cout << "Enter your Gender 'M' for Male 'F' for Female \n";
+	cin >> gender;
+
 	if (result > 50)
 		cout << "PASS\n";
 	else
 		cout << "FAIL\n";
+
+	if (gender == 'F')
+		cout << "You Are Femallle\n";
+	else 
+		cout << "You Are Male\n";
 
 
 	return 0;
