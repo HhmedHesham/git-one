4c2d7c970d7d09d4a048a3ef2e31921ba7c2f7fd
iamahmedh <heshama793@gmail.com>
2020-10-19 23:25:28 +0200

    2 adding conditoins to our test

--- test.cpp
+++ test.cpp
@@ -3,8 +3,16 @@
 using namespace std;
 
 int main() {
+	int result{ 0 };
 	
-	cout << "Test\n";
+	cout << "Enter your result to know your Grade\n";
+	cin >> result;
+	
+	if (result > 50)
+		cout << "PASS\n";
+	else
+		cout << "FAIL\n";
+
 
 	return 0;
 
