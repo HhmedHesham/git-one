d763a64a375d1e04a1ed29f713509a00f536128f
iamahmedh <heshama793@gmail.com>
2020-10-20 08:38:35 +0200

    What happen when you bacome the main source of bain?

--- test.cpp
+++ test.cpp
@@ -3,24 +3,8 @@
 using namespace std;
 
 int main() {
-	//changes not commited so its not the master
-	int result{ 0 };
-	cout << "Enter your result to know your Grade\n";
-	cin >> result;
-
-	char gender{};
-	cout << "Enter your Gender 'M' for Male 'F' for Female \n";
-	cin >> gender;
-
-	if (result > 50)
-		cout << "PASS\n";
-	else
-		cout << "FAIL\n";
-
-	if (gender == 'F')
-		cout << "You Are Femallle\n";
-	else 
-		cout << "You Are Male\n";
+	
+	cout << "What Happen when you become the main source of pain!?\n";
 
 
 	return 0;
